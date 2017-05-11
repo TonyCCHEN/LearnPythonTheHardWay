69eab6b7cc59dfec7ecfc2e9285b134f5c5a0a8c
TonyCChen <sinsankimo@gmail.com>
2017-05-12 00:27:04 +0800

    ex3.py practiced in Python2

---
+++ ex3.py
@@ -0,0 +1,24 @@
+#Exercise 3 Numbers and Math
+# Reference: https://learnpythonthehardway.org/book/ex3.html
+
+print "I will count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Rooster", 100 - 25.0 *3 % 4
+
+print "Now I will count the eggs:"
+
+print 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it it's False."
+
+print "It is greater?", 5 > -2
+print "It is greater or equal?", 5 >= -2
+print "It is less or equal?", 5 <= -2
